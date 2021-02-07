29db31c3eacf84945203720423111750f67c53e0
Aaron Gonzales <aaroncgonzales.dev@gmail.com>
2021-02-08 00:39:20 +0800

    Added Drama and Episode classes

--- dramakul/sites/base_classes.py
+++ dramakul/sites/base_classes.py
@@ -29,3 +29,30 @@ class SearchResult:
 
     def get_info(self):
         return self.site.get_info(self.url)
+
+    def __str__(self):
+        return self.title
+
+
+class Drama:
+    def __init__(self, site: Site, title: str, url: str, meta={}):
+        self.site = site
+        self.title = title
+        self.url = url
+        self.meta = meta
+        self.episodes = []
+
+    def __str__(self):
+        return self.title
+
+
+class Episode:
+    def __init__(self, site: Site, drama: Drama, episode_number: str, url: str, meta={}):
+        self.site = site
+        self.drama = drama
+        self.episode_number = episode_number
+        self.url = url
+        self.meta = meta
+
+    def __str__(self):
+        return f"{self.drama.title} - {self.episode_number}"
