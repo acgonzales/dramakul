7f6db7ee9ad6cab451ad72fb548fe52e50339800
Aaron Gonzales <aaroncgonzales.dev@gmail.com>
2021-02-08 01:10:15 +0800

    Added get_site method

--- dramakul/sites/__init__.py
+++ dramakul/sites/__init__.py
@@ -2,3 +2,9 @@ from dramakul.sites.base_classes import Site, SearchResult, Drama, Episode
 from dramakul.sites.dramacool9 import Dramacool9
 
 SITES = [Dramacool9]
+
+
+def get_site(url, *args, **kwargs):
+    for site in SITES:
+        if site.domain in url or site.name.lower() in url:
+            return site()
