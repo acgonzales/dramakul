32c4763f35729be4d61b0eaaa0ae77551e1242cb
Aaron Gonzales <aaroncgonzales.dev@gmail.com>
2021-02-08 00:42:18 +0800

    Added XStreamCDN extractor

--- dramakul/extractors/__init__.py
+++ dramakul/extractors/__init__.py
@@ -1,4 +1,5 @@
-from dramakul.extractors.cloud9 import Cloud9
 from dramakul.extractors.base_extractor import Extractor
+from dramakul.extractors.cloud9 import Cloud9
+from dramakul.extractors.xstreamcdn import XStreamCDN
 
-EXTRACTORS = [Cloud9]
+EXTRACTORS = [Cloud9, XStreamCDN]
---
+++ dramakul/extractors/xstreamcdn.py
@@ -0,0 +1,24 @@
+from dramakul.extractors import Extractor
+
+
+class XStreamCDN(Extractor):
+    name = "XStreamCDN"
+    regexes = ["xstreamcdn", "fcdn.stream", "https://fcdn.stream"]
+
+    def __init__(self, url, *args, **kwargs):
+        super().__init__(url, *args)
+
+    def extract_url(self):
+        data = self.session.post(
+            "https://www.xstreamcdn.com/api/source/" + self.url.split("/")[-1]).json()
+        data = data["data"]
+
+        sources = {}
+
+        for source in data:
+            sources[source["label"]] = source["file"]
+
+        return {
+            "sources": sources,
+            "preferred_quality": list(sources.keys())[-1]
+        }
