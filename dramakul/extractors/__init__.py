d0d4bec55bf89c63533cd239a2d881c1db9822df
Aaron Gonzales <aaroncgonzales.dev@gmail.com>
2021-02-08 01:05:17 +0800

    Return instance instead of class in get_extractor

--- dramakul/extractors/__init__.py
+++ dramakul/extractors/__init__.py
@@ -5,8 +5,8 @@ from dramakul.extractors.xstreamcdn import XStreamCDN
 EXTRACTORS = [Cloud9, XStreamCDN]
 
 
-def get_extractor(url):
+def get_extractor(url, *args, **kwargs):
     for extractor in EXTRACTORS:
         for regex in extractor.regexes:
             if regex in url:
-                return extractor
+                return extractor(url, *args, **kwargs)
