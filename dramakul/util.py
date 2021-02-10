fc69e4a4a0350cb8f6c4c42be8f49eb2ccda9f8d
Aaron Gonzales <aaroncgonzales.dev@gmail.com>
2021-02-10 17:15:44 +0800

    Mount HTTPAdapter to session

--- dramakul/util.py
+++ dramakul/util.py
@@ -1,12 +1,18 @@
 from bs4 import BeautifulSoup
 import jsbeautifier
 import requests
+from requests.adapters import HTTPAdapter
 
 from dramakul.constants import get_random_headers
 
 
-def create_session(headers=get_random_headers()):
+def create_session(headers=get_random_headers(), max_retries=5):
+    adapter = HTTPAdapter(max_retries=max_retries)
+
     session = requests.Session()
+    session.mount("http://", adapter)
+    session.mount("https://", adapter)
+
     session.headers.update(headers)
     return session
 
